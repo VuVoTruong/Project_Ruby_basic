eb6e9f34c53c03756bd32e2c055f71a86eba5adc
VuVT <truongvuuit@gmail.com>
2022-10-16 12:15:54 +0700

    Comment

--- Bai5/Bai5.rb
+++ Bai5/Bai5.rb
@@ -41,3 +41,10 @@ else
   puts "mảng #{arr2} KHÔNG phải là mảng con của #{arr1}"
   
 end
+
+# A có vài góp ý 
+# 1. mảng [1, 6, -1, 10] là mảng con của [5, 6, 22, 26, 1, -1, 8, 10] -> này là kq của ct e put ra ah, và rõ ràng kg cùng thứ tự rồi e.
+# 2. arr1 hay biến m, n cần được refactor lại, để dễ đọc hơn
+# 3. Nếu e order 2 mảng rồi so sánh thì giải thuật kg đúng rồi nhé e
+# 4. isSubset -> mình dùng format của ruby thì thường là is_subset? nhé
+# 5. Hơi bị nhiều if else ah
